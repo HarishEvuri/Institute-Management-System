6d36ee26e9e2959d6519df7173383bbd11854152
Harish Evuri <evuri.harish.cd.cse19@itbhu.ac.in>
2021-10-23 16:55:27 +0530

    First Commit

---
+++ src/main/java/com/harish/institutemanagement/InstituteManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.harish.institutemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstituteManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstituteManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/harish/institutemanagement/InstituteManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harish.institutemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstituteManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
